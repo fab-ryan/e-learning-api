e0f07fac9a267bf1e2d788c81262ce447d254110
fab-ryan <royalfabrice1234@gmail.com>
2024-05-07 23:57:42 +0200

    adding testing

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AppModule } from './app.module';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -17,6 +18,13 @@ describe('AppController', () => {
   describe('root', () => {
     it('should return "Hello World!"', () => {
       expect(appController.getHello()).toBe('Hello World!');
+    });
+  });
+
+  describe('module testing', () => {
+    it("should return 'Hello World!'", () => {
+      const appModule = new AppModule();
+      expect(appModule).toBeDefined();
     });
   });
 });
