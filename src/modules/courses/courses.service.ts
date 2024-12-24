69bac15d7813c386e782d5e4187c14c5b8e9e50d
fab-ryan <royalfabrice1234@gmail.com>
2024-12-24 21:16:07 +0200

    course implementation

--- src/modules/courses/courses.service.ts
+++ src/modules/courses/courses.service.ts
@@ -240,7 +240,6 @@ export class CoursesService {
       const courses = await this.courseRepository.find({
         where: { creator },
       });
-
       return this.responseService.Response({
         data: courses,
         message: 'Courses fetched successfully',
