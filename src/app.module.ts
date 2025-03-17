736ed0fe9f8268e50d034c6c9c860a7b9693e533
fab-ryan <royalfabrice1234@gmail.com>
2025-03-18 00:54:07 +0200

    fixing the issues with login all

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,6 +12,7 @@ import { PrometheusModule } from './modules/prometheus/prometheus.module';
 import { EnrollsModule } from './modules/enrolls/enrolls.module';
 
 
+
 @Module({
   imports: [
     SharedModule,
